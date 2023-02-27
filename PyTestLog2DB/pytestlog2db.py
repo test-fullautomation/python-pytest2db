04f67b350cf55a2c1088f8402ea8c6024b740e68
ntd1hc <ngoan.tranduy@vn.bosch.com>
2023-02-27 11:36:48 +0700

    --append mode with not existing UUID will raise error

--- PyTestLog2DB/pytestlog2db.py
+++ PyTestLog2DB/pytestlog2db.py
@@ -331,7 +331,7 @@ Avalable arguments in command line:
    - `database` : database name.
    - `--recursive` : if True, then the path is searched recursively for *.xml files to be imported.
    - `--dryrun` : if True, then verify all input arguments (includes DB connection) and show what would be done.
-   - `--append` : if True, then allow to append new result(s) to existing execution result UUID which is provided by -UUID argument.
+   - `--append` : if True, then allow to append new result(s) to existing execution result UUID which is provided by --UUID argument.
    - `--UUID` : UUID used to identify the import and version ID on TestResultWebApp.
    - `--variant` : variant name to be set for this import.
    - `--versions` : metadata: Versions (Software;Hardware;Test) to be set for this import.
@@ -360,8 +360,8 @@ Avalable arguments in command line:
    cmdlineparser.add_argument('database', type=str, help='database schema for database login.')
    cmdlineparser.add_argument('--recursive', action="store_true", help='if set, then the path is searched recursively for output files to be imported.')
    cmdlineparser.add_argument('--dryrun', action="store_true", help='if set, then verify all input arguments (includes DB connection) and show what would be done.')
-   cmdlineparser.add_argument('--append', action="store_true", help='is used in combination with -UUID <UUID>.' +\
-                              ' If set, allow to append new result(s) to existing execution result UUID in -UUID argument.')
+   cmdlineparser.add_argument('--append', action="store_true", help='is used in combination with --UUID <UUID>.' +\
+                              ' If set, allow to append new result(s) to existing execution result UUID in --UUID argument.')
    cmdlineparser.add_argument('--UUID', type=str, help='UUID used to identify the import and version ID on webapp.' + \
                               ' If not provided PyTestLog2DB will generate an UUID for the whole import.')
    cmdlineparser.add_argument('--variant', type=str, help='variant name to be set for this import.')
@@ -1053,7 +1053,7 @@ Flow to import PyTest results to database:
    * `database` : database name.
    * `recursive` : if True, then the path is searched recursively for log files to be imported.
    * `dryrun` : if True, then just check the RQM authentication and show what would be done.
-   * `append` : if True, then allow to append new result(s) to existing execution result UUID which is provided by -UUID argument.
+   * `append` : if True, then allow to append new result(s) to existing execution result UUID which is provided by --UUID argument.
    * `UUID` : UUID used to identify the import and version ID on TestResultWebApp.
    * `variant` : variant name to be set for this import.
    * `versions` : metadata: Versions (Software;Hardware;Test) to be set for this import.
@@ -1195,38 +1195,40 @@ Flow to import PyTest results to database:
       _tbl_result_jenkinsurl     = ""
       _tbl_result_reporting_qualitygate = ""
 
-      # Process new test result
-      if not Logger.dryrun:
-         db.sCreateNewTestResult(_tbl_prj_project,
-                                 _tbl_prj_variant,
-                                 _tbl_prj_branch,
-                                 _tbl_test_result_id,
-                                 _tbl_result_interpretation,
-                                 _tbl_result_time_start,
-                                 _tbl_result_time_end,
-                                 _tbl_result_version_sw_target,
-                                 _tbl_result_version_sw_test,
-                                 _tbl_result_version_hardware,
-                                 _tbl_result_jenkinsurl,
-                                 _tbl_result_reporting_qualitygate)
-      Logger.log("Created test execution result for version '%s' successfully: %s"%(_tbl_result_version_sw_target,str(_tbl_test_result_id)))
-   except Exception as reason:
-      # MySQL error code:
-      # Error Code   | SQLSTATE	|Error	      |Description                     
-      # -------------+-----------+--------------+-------------------------------
-      # 1062	      | 23000	   |ER_DUP_ENTRY	|Duplicate entry '%s' for key %d
-      if reason.args[0] == 1062:
-         # check -append argument
+      # Check the UUID is existing or not
+      error_indent = len(Logger.prefix_fatalerror)*' '
+      if db.bExistingResultID(_tbl_test_result_id):
          if args.append:
             Logger.log(f"Append to existing test execution result UUID '{_tbl_test_result_id}'.")
          else:
-            error_indent = len(Logger.prefix_fatalerror)*' '
             Logger.log_error(f"Execution result with UUID '{_tbl_test_result_id}' is already existing. \
                \n{error_indent}Please use other UUID (or remove '--UUID' argument from your command) for new execution result. \
                \n{error_indent}Or add '--append' argument in your command to append new result(s) to this existing UUID.", 
                fatal_error=True)
       else:
-         Logger.log_error("Could not create new execution result. Reason: %s"%reason, fatal_error=True)
+         if args.append:
+            Logger.log_error(f"Execution result with UUID '{_tbl_test_result_id}' is not existing for appending.\
+               \n{error_indent}Please use an existing UUID to append new result(s) to that UUID. \
+               \n{error_indent}Or remove '--append' argument in your command to create new execution result with given UUID.", 
+               fatal_error=True)
+         else:
+            # Process new test result
+            if not Logger.dryrun:
+               db.sCreateNewTestResult(_tbl_prj_project,
+                                       _tbl_prj_variant,
+                                       _tbl_prj_branch,
+                                       _tbl_test_result_id,
+                                       _tbl_result_interpretation,
+                                       _tbl_result_time_start,
+                                       _tbl_result_time_end,
+                                       _tbl_result_version_sw_target,
+                                       _tbl_result_version_sw_test,
+                                       _tbl_result_version_hardware,
+                                       _tbl_result_jenkinsurl,
+                                       _tbl_result_reporting_qualitygate)
+            Logger.log("Created test execution result for version '%s' successfully: %s"%(_tbl_result_version_sw_target,str(_tbl_test_result_id)))
+   except Exception as reason:
+      Logger.log_error("Could not create new execution result. Reason: %s"%reason, fatal_error=True)
 
    for suite in pytest_result.iterchildren("testsuite"):
       process_suite(db, suite, _tbl_test_result_id, dConfig)
